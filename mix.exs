02de1be792bc404b360bd23303e2258b402e1db4
John Green <kazlu@protonmail.com>
2023-03-06 16:40:46 -0500

    Add phoenix_view as a separate depedency

--- mix.exs
+++ mix.exs
@@ -47,6 +47,7 @@ defmodule Bookmark.MixProject do
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.18.7"},
+      {:phoenix_view, "~> 2.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.7"},
       {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
