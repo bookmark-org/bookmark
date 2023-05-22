5735ea66ae96b61fc9db76cd016866a47b84d402
Leo Bazan <leo.bazan.x@gmail.com>
2023-05-22 12:25:42 -0300

    User profile: Add link to archives

--- lib/bookmark_web/views/page_view.ex
+++ lib/bookmark_web/views/page_view.ex
@@ -4,18 +4,20 @@ defmodule BookmarkWeb.PageView do
     a = assigns[:a]
     conn = assigns[:conn]
     ~E"""
-    <article class="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm dark:border-gray-800 dark:bg-black dark:shadow-gray-700/25 h-[35rem]" >
-      <%= img_tag(Routes.static_path(conn, "/archive/archive/" <> a.name <> "/screenshot.png"), alt: "Archive", class: "h-56 w-full object-cover object-top")%>
-      <div class="p-4 sm:p-6">
-        <time class="block text-sm text-gray-400 dark:text-gray-400">
-          <%= NaiveDateTime.to_string(a.updated_at) %>
-        </time>
-        <h3 class="mt-0.5 text-lg text-white"><%= a.title %>y</h3>
-        <p class="mt-2 line-clamp-3 text-base/relaxed text-gray-400 dark:text-gray-500">
-          <%= a.summary %>
-        </p>
-      </div>
-    </article>
+    <a href=<%= "/archive/" <> a.name %> >
+      <article class="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm dark:border-gray-800 dark:bg-black dark:shadow-gray-700/25 h-[35rem]" >
+        <%= img_tag(Routes.static_path(conn, "/archive/archive/" <> a.name <> "/screenshot.png"), alt: "Archive", class: "h-56 w-full object-cover object-top")%>
+        <div class="p-4 sm:p-6">
+          <time class="block text-sm text-gray-400 dark:text-gray-400">
+            <%= NaiveDateTime.to_string(a.updated_at) %>
+          </time>
+          <h3 class="mt-0.5 text-lg text-white"><%= a.title %>y</h3>
+          <p class="mt-2 line-clamp-3 text-base/relaxed text-gray-400 dark:text-gray-500">
+            <%= a.summary %>
+          </p>
+        </div>
+      </article>
+    </a>
     """
   end
 end
