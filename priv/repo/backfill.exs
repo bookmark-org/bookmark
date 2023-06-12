e708c439f72baed4961d39d86ba099a0fb15da0a
Leo Bazan <leo.bazan.x@gmail.com>
2023-06-12 17:25:53 -0300

    Backfill script: Handle error

--- priv/repo/backfill.exs
+++ priv/repo/backfill.exs
@@ -2,18 +2,24 @@
 #
 #     mix run priv/repo/backfill.exs
 
+require Logger
+
 alias Bookmark.Repo
 alias Bookmark.Archives.Archive
 alias Bookmark.Archives
 
 # Backfill
 Repo.all(Archive) |> Enum.each(fn a ->
-  if a.summary == nil || a.summary == "Summary not available" do
-    Archives.add_summary(a)
-  end
+  try do
+    if a.summary == nil || a.summary == "Summary not available" do
+      Archives.add_summary(a)
+    end
 
-  if a.title == nil do
-    Archive.changeset(a, %{title: Archives.get_title(a.name)})
-    |> Repo.update()
+    if a.title == nil do
+      Archive.changeset(a, %{title: Archives.get_title(a.name)})
+      |> Repo.update()
+    end
+  rescue e ->
+    Logger.error(Exception.format(:error, e, __STACKTRACE__))
   end
 end)
