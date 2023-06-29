3fef73ad2209e8ffda43e63084d20253ad1c00a7
Leo Bazan <leo.bazan.x@gmail.com>
2023-06-29 17:12:04 -0300

    Fix wrong domains not allowed

--- lib/bookmark/utils.ex
+++ lib/bookmark/utils.ex
@@ -5,4 +5,23 @@ defmodule Bookmark.Utils do
     |> Map.put(:query, nil)
     |> URI.to_string()
   end
+
+  # Extracts the domain from a url
+  # Example: www.sub1.sub2.sub3.domain.com/path -> domain.com
+  def get_domain(url) do
+    case URI.parse(url) do
+      %{authority: nil} ->
+        url
+
+      %{authority: host} ->
+        # sub1.sub2.sub3.domain.com -> 3
+        subdomains = (host |> String.split(".") |> length()) - 2
+        if subdomains >= 1 do
+          subdomains = String.split(host, ".", parts: subdomains + 1)
+          List.last(subdomains)
+        else
+          host
+        end
+    end
+  end
 end
